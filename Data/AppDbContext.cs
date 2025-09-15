7b0b1ade0f83fcaa9ad29a9ab9bc21ed894cfa40
Mariana Parada <a2024111530@estudiantes.upsa.edu.bo>
2025-09-14 20:43:35 -0400

    agregar gitgnore

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,12 +3,8 @@ using prueba.API.Model;
 
 namespace prueba.API.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
-
         public DbSet<Meals> Meals { get; set; }
     }
 }
